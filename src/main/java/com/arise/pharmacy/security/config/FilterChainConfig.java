dc07d4e8b11ca217c4c8f55c40b990a60e5c3a1e
nicholas-hgit <nicholas.hlungwane@gmail.com>
2024-06-02 02:46:35 +0200

    initial commit

---
+++ src/main/java/com/arise/pharmacy/security/config/FilterChainConfig.java
@@ -0,0 +1,26 @@
+package com.arise.pharmacy.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class FilterChainConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        req -> req
+                                .anyRequest()
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+}
