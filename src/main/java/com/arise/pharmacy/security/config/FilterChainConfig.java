7c47416f5979ea44dae782fdb80b685a131325aa
nicholas-hgit <nicholas.hlungwane@gmail.com>
2024-06-05 16:47:33 +0200

    enabled stateless session management

--- src/main/java/com/arise/pharmacy/security/config/FilterChainConfig.java
+++ src/main/java/com/arise/pharmacy/security/config/FilterChainConfig.java
@@ -1,15 +1,23 @@
 package com.arise.pharmacy.security.config;
 
+import com.arise.pharmacy.security.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class FilterChainConfig {
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -17,9 +25,13 @@ public class FilterChainConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         req -> req
-                                .anyRequest()
+                                .requestMatchers(HttpMethod.POST,"/api/v1/auth","/api/v1/users/register")
                                 .permitAll()
-                );
+                                .anyRequest()
+                                .authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(c -> c.sessionCreationPolicy(STATELESS));
 
         return http.build();
     }
